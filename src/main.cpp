1fa79eab41d81aa28954e4c8888160a0179f793f
Omer Faruk Istikbal <ofarukistikbal@hotmail.com>
2023-02-04 11:27:50 +0300

    Add GLM

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
+#include <glm/vec3.hpp>
+
+using point3 = glm::vec<3, double, glm::defaultp>;   // 3D point
+using color = glm::vec<3, double, glm::defaultp>;  // RGB color
+
+void write_color(std::ostream& out, color pixel_color) {
+    // Write the translated [0,255] value of each color component.
+    out << static_cast<int>(255.999 * pixel_color.x) << ' '
+        << static_cast<int>(255.999 * pixel_color.y) << ' '
+        << static_cast<int>(255.999 * pixel_color.z) << '\n';
+}
 
 int main() {
 
@@ -14,15 +25,8 @@ int main() {
     for (int j = image_height - 1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.25;
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            color pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+            write_color(std::cout, pixel_color);
         }
     }
 }
