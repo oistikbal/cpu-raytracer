8ce0fee690d8e147f0279c8d99b6d32a16695577
Omer Faruk Istikbal <ofarukistikbal@hotmail.com>
2023-02-04 13:52:48 +0300

    Add glfw and render with opengl

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,26 @@
+#include <GLFW/glfw3.h>
 #include <iostream>
+#include <sstream>
 
 #include "vec3.h"
 #include "ray.h"
 
-#include <iostream>
+// Image
+const auto aspect_ratio = 16.0 / 9.0;
+const int image_width = 640;
+const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+// Camera
+
+auto viewport_height = 2.0;
+auto viewport_width = aspect_ratio * viewport_height;
+auto focal_length = 1.0;
+
+auto origin = point3(0, 0, 0);
+auto horizontal = vec3(viewport_width, 0, 0);
+auto vertical = vec3(0, viewport_height, 0);
+auto lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - vec3(0, 0, focal_length);
+
 
 color ray_color(const ray& r) {
     vec3 unit_direction = glm::normalize(r.direction());
@@ -11,38 +28,55 @@ color ray_color(const ray& r) {
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
-int main() {
-
-    // Image
-    const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 640;
-    const int image_height = static_cast<int>(image_width / aspect_ratio);
-
-    // Camera
-
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
-
-    auto origin = point3(0, 0, 0);
-    auto horizontal = vec3(viewport_width, 0, 0);
-    auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - vec3(0, 0, focal_length);
-
-    // Render
-
-    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
-
+void render()
+{
     for (int j = image_height - 1; j >= 0; --j) {
-        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
             auto u = double(i) / (image_width - 1);
             auto v = double(j) / (image_height - 1);
             ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
             color pixel_color = ray_color(r);
-            write_color(std::cout, pixel_color);
+            glEnable(GL_SCISSOR_TEST);
+            glScissor(i, j, 1, 1);
+            glClearColor(pixel_color.r, pixel_color.g, pixel_color.b, 1);
+            glClear(GL_COLOR_BUFFER_BIT);
+            glDisable(GL_SCISSOR_TEST);
         }
     }
+}
 
-    std::cerr << "\nDone.\n";
+int main()
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(image_width, image_height, "CPU Raytracer", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    glClear(GL_COLOR_BUFFER_BIT);
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        render();
+
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
